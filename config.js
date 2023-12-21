21d5d9667bd96cc7e73e0f6f2f62f4714e7a3930
HoHP <thivvnlh@gmail.com>
2023-12-22 00:26:53 +0700

    feaT: fix

---
+++ config.js
@@ -0,0 +1,46 @@
+// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
+var config = {
+  // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
+  // 每句话尽量不要超过15个字,不然展示效果可能不太好
+  texts: [
+    'Chúc mừng sinh nhật cục zàng của anh',
+    'Tuổi mới tràn đầy sức khoẻ',
+    'Công việc thuận lợi',
+    'Yêu anh hơn',
+    'Dù thế nào đi nữa',
+    'Dù em như thế nào!',
+    'Anh vẫn bên cạnh và ủng hộ em theo con đường em chọn',
+    'Không biết đúng hay sai nhưng anh tin em sẽ làm được',
+    'Mình còn trẻ mình được quyền phép thử',
+    'Sau này có như thế nào cũng phải mỉm cười vì đây là giấc mơ của em',
+    'Anh xuất hiện trong đời để giúp em thực hiện giấc mơ đó',
+    'Mình cùng nhau cố gắng em nhé ❤️',
+    'Cuối cùng thì...',
+    'Happy birthday to you! 🎂🎂🎂',
+    'I love you with all I have! 😘',
+  ],
+  /**
+   * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
+   * "对应上面的文字, 要完全一样" : "图片地址, 可以把图片放在imgs文件夹中"
+   * 例如
+   * "心爱的小可爱": "./imgs/xiaokeai.jpg"
+   *
+   * 如果不要图片的话, 直接在每行开头写两个斜杠注释即可, 例如下面的 "今天是你的生日" 的图片就不会展示了:)
+   * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
+   */
+  // imgs: {
+  //   'Yêu anh hơn': './imgs/xiaokeai.png',
+  //   // 今天是你的生日: './imgs/birthday.jpg',
+  // },
+  // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
+  desc: {
+    turn_on: '🎬 Bật đèn lên. Trả lại tâm trí tui đây! 🎬',
+    play: '🎶 Lên nhạc 🎶',
+    bannar_coming: 'Băng rôn đâu rồi?',
+    balloons_flying: '🎈 Bóng bay đi đâu đó 🎈',
+    cake_fadein: '🎂 Cái bánh này 🎂',
+    light_candle: '🕯️ Đốt nến lên 🕯️',
+    wish_message: '🎂🎇 Chúc mừng sinh nhật! Cục vàng của anh (Dương Khiết Di) 🎂🎇',
+    story: '🥰 Lời nhắn khùng điên của anh nè 🥰',
+  },
+};
